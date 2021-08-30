262c0c768dfb2a649613c53805a86795e78d9458
salis <salisshuaibu11@gmail.com>
2021-08-30 01:25:11 +0000

    Configure Hot Module Replacement for the React component tree

--- src/index.js
+++ src/index.js
@@ -2,20 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import configureStore from "./store/configureStore";
 const store = configureStore();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// Save a reference to the root element for reuse
+const rootEl = document.getElementById("root");
+
+// Create a reusable render method that we can call more than once
+let render = () => {
+  // Dynamically import our main App component, and render it
+  const App = require("./App").default;
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    rootEl
+  );
+};
+
+if (process.env.NODE_ENV !== "production") {
+  if (module.hot) {
+    // Support hot reloading of components.
+    // Whenever the App component file or one of its dependencies
+    // is changed, re-import the updated component and re-render it
+    module.hot.accept("./App", () => {
+      setTimeout(render);
+    });
+  }
+}
+
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
